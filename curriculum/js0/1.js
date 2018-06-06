2cf4a83ca995878aa0b627c44883533beee5f66f
Justin Drake <jdrake421@garagescript.org>
2018-06-05 18:33:01 -0700

    js0 1.js

--- curriculum/js0/1.js
+++ curriculum/js0/1.js
@@ -1,12 +1,12 @@
-/**
+/**.
  * Takes in 2 numbers, returns their sum
  * @param {number} a
  * @param {number} b
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a,b) => {
+  return a+b;
 };
 
 module.exports = {
